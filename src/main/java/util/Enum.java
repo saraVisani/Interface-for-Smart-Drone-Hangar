82faaa78c5086559a392e27ca41b14d2309cfea9
Sara Visani <sara.visani7@studio.unibo.it>
2025-12-19 14:43:00 +0100

    Deleted spaces

--- src/main/java/util/Enum.java
+++ src/main/java/util/Enum.java
@@ -5,4 +5,36 @@ public class Enum {
         ORDERS,
         LOGS
     }
+
+    public enum RequestType {
+
+        TAKE_OFF(false),
+        LANDING(false),
+        LOGS(false);
+
+        private boolean enabled;
+
+        // costruttore dell'enum
+        RequestType(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        // getter
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        // utility
+        public void enable() {
+            this.enabled = true;
+        }
+
+        public void disable() {
+            this.enabled = false;
+        }
+
+        public void toggle() {
+            this.enabled = !this.enabled;
+        }
+    }
 }
